ad34b071a0388b118faa8b4530fd0aafbfcdbe6c
Martin Bing <bing.martin.photography@gmail.com>
2016-04-11 10:44:12 +0200

    Add files to make wiki search work

--- js/part1.js
+++ js/part1.js
@@ -1,5 +1,5 @@
-var $input = $('#input'),
-	$result = $('#resutls');
+var $input = $('input'),
+	$results = $('#results');
 
 var keyups = Rx.Observable
 				.fromEvent($input, 'keyup')
@@ -9,3 +9,30 @@ var keyups = Rx.Observable
 var throttled = keyups.throttle(500);
 var distinct = throttled.distinctUntilChanged();
 
+function searchWikipedia (term) {
+    return $.ajax({
+        url: 'http://en.wikipedia.org/w/api.php',
+        dataType: 'jsonp',
+        data: {
+            action: 'opensearch',
+            format: 'json',
+            search: term
+        }
+    }).promise();
+}
+
+var suggestions = distinct.flatMapLatest(searchWikipedia);
+
+suggestions.subscribe(data => {
+    var res = data[1];
+
+    /* Do something with the data like binding */
+    $results.empty();
+
+    $.each(res, (_, value) => $('<li>' + value + '</li>').appendTo($results));
+}, error => {
+    /* handle any errors */
+    $results.empty();
+
+    $('<li>Error: ' + error + '</li>').appendTo($results);
+});
